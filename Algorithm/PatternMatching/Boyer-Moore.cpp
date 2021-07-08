94c160764e88e2e5b696e784e2f5bbc88d92cdac
Kitbert <kitbert77@gmail.com>
2021-07-08 15:29:48 +0700

    Fix BoyerMoore function.
    - Fix bug.
    - Move print function from main to BoyerMoore().

--- Algorithm/PatternMatching/Boyer-Moore.cpp
+++ Algorithm/PatternMatching/Boyer-Moore.cpp
@@ -2,7 +2,7 @@
 #include <string.h> 
 using namespace std;
 
-int BoyerMoore(string text, string pattern)
+void BoyerMoore(string text, string pattern)
 {
     int textIndex, patternIndex;
     int badCharacterSkipIndex = 0;  //Bad Character rule
@@ -11,59 +11,56 @@ int BoyerMoore(string text, string pattern)
     //Pattern will not match with the text
     if(pattern.length() >= text.length())
     {
-        return -1;
+        cout << "Pattern does not exist!";
     } 
 
     textIndex = pattern.length() - 1; 
     
     while(text.length() - 1 >= textIndex)
     {
-        cout << "go\n";
-
         patternIndex = pattern.length() - 1;
-        
-        while(patternIndex >= 0)
+
+        if(pattern[patternIndex] == text[textIndex])
         {
-            cout << "lets\n";
-            cout << "Pattern index: " << patternIndex << " text index: " << textIndex << "\n";
-
-            if(pattern[patternIndex] == text[textIndex])
+            while(pattern[patternIndex] == text[textIndex])
             {
-                cout << "try\n";
-
                 patternIndex--;
 
                 if(patternIndex == -1)
-                {
-                    return textIndex;
+                {      
+                    cout << text << "\n";
+                        
+                    for(int i = 0; i < textIndex; i++)
+                    {
+                        cout << " ";
+                    }
+
+                    cout << pattern << "\n";
+                    return;
                 }
 
                 textIndex--;
+            }
+
+        } else
+        {
+            while(pattern[patternIndex] != text[textIndex] && patternIndex != -1)
+            {
+                patternIndex--;
+            }
+
+            if(patternIndex == -1)
+            {
+                badCharacterSkipIndex = pattern.length();
                 
             } else
             {
-                cout << "more\n";
-
-                while(pattern[patternIndex] != text[textIndex] && patternIndex != -1)
-                {
-                    patternIndex--;
-                }
-
-                if(patternIndex == -1)
-                {
-                    badCharacterSkipIndex = pattern.length() - 1;
-                    
-                } else
-                {
-                    badCharacterSkipIndex = (pattern.length() - 1) - patternIndex;    
-                }
-                
-                textIndex += badCharacterSkipIndex;
+                badCharacterSkipIndex = (pattern.length() - 1) - patternIndex;    
             }
+            
+            textIndex += badCharacterSkipIndex;
         }
     }
-
-    return -1;
 }
 
 int main()
@@ -77,22 +74,5 @@ int main()
     cout << "Input the pattern: ";
     getline(cin, pattern);
 
-    matchIndex = BoyerMoore(text, pattern);
-
-    if(matchIndex == -1)
-    {
-        cout << "Pattern does not exist!\n";
-
-    } else
-    {
-        cout << "Pattern founded!\n";
-        cout << text << "\n";
-
-        for(int i = 0; i < matchIndex; i++)
-        {
-            cout << " ";
-        }
-
-        cout << pattern << "\n";
-    }
+    BoyerMoore(text, pattern);
 }
