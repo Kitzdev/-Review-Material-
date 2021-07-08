1c6e2c045cefaf6714218a984a083081339b8aea
Kitbert <kitbert77@gmail.com>
2021-07-08 15:29:08 +0700

    Make a new directory KotlinFundamental.
    - Add HelloKotlin file.
    - Add DataTypesAndVariable file.

---
+++ Kotlin/KotlinFundamental/src/main/kotlin/kotlinFundamental/DataTypesAndVariable.kt
@@ -0,0 +1,6 @@
+package kotlinFundamental
+
+fun main()
+{
+
+}
---
+++ Kotlin/KotlinFundamental/src/main/kotlin/kotlinFundamental/HelloKotlin.kt
@@ -0,0 +1,13 @@
+package kotlinFundamental
+
+//main function
+fun main()
+{
+    println("Hello Kotlin!")
+
+    val name = "Kitbert"
+
+    print("Hello my name is ")
+    println(name)
+    print(if (true) "Stay Positive" else "Stay There" )
+}
