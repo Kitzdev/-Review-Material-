4d3ef22cd0153ceabdee0a1eec95b3565048eef2
Kitbert <kitbert77@gmail.com>
2021-08-16 15:53:08 +0700

    Add Splash Screen.

--- AndroidStudioProjects/WorkshopPemrogramanAndroid/app/src/main/java/com/android/workshoppemrogramanandroid/MainActivity.kt
+++ AndroidStudioProjects/WorkshopPemrogramanAndroid/app/src/main/java/com/android/workshoppemrogramanandroid/MainActivity.kt
@@ -1,11 +1,11 @@
-package com.example.letscompare_vgaseries
+package com.android.workshoppemrogramanandroid
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
